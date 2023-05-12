ca505a9712723ee5a8f93ad1738f665370115525
root <matiassotelo171097@gmail.com>
2023-05-12 10:00:04 -0700

    0-binary_tree_node v1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,39 +4,38 @@
  * binary_tree_node - a function that creates a binary tree node
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
- * 
+ *
  * Return: a binary tree
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *temp;
+binary_tree_t *temp;
 
-    temp = maloc(sizeof(binary_tree_t));
-    if (temp == NULL)
-    {
-        free(temp);
-        return (NULL);
-    }
-    
-    temp->n = value;
-    temp->parent = parent;
+temp = malloc(sizeof(binary_tree_t));
+if (temp == NULL)
+{
+free(temp);
+return (NULL);
+}
 
-    if (parent == NULL)
-    {
-        temp->left = NULL;
-        temp->right = NULL;
+temp->n = value;
+temp->parent = parent;
 
-    }
+if (parent == NULL)
+{
+temp->left = NULL;
+temp->right = NULL;
 
-    if (parent->left != NULL && parent->right != NULL)
-    {
-        return (NULL);
-    }
-    
-    if (parent->left == NULL, parent->right == NULL)
-        parent->left = value;
-    if (parent->left != NULL)
-        parent->right = value;
-    free(temp);
-    return(parent);       
+}
+
+if (parent->left != NULL && parent->right != NULL)
+{
+return (NULL);
+}
+if (parent->left == NULL, parent->right == NULL)
+parent->left->n = value;
+if (parent->left != NULL)
+parent->right->n = value;
+free(temp);
+return (parent);
 }
